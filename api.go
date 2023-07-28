969e5afe1f3874f427c9072fd28a05512c35c526
lazy_owl <slimsly11@gmail.com>
2023-07-28 15:45:54 +0600

    feat(server): run async method new

--- api.go
+++ api.go
@@ -6,6 +6,10 @@ func (app *App) Run(port string) {
 	log.Fatal(app.server.Start(port))
 }
 
+func (app *App) RunAsync(port string) {
+	go app.Run(port)
+}
+
 func (app *App) Close() {
 	log.Error(app.server.Close(), "Shutdown gRPC server error")
 }
